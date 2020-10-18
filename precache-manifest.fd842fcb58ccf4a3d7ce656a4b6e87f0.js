98a3da605f4024d8ee5fc7961be19ead00566634
aryan007007 <aryanasthana211@gmail.com>
2020-10-19 00:18:24 +0530

    Updates

---
+++ precache-manifest.fd842fcb58ccf4a3d7ce656a4b6e87f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b782baddb85767c6a2e1c5367cba386a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "89abc366f9dce1ab17a7",
+    "url": "/monsters-rolodex/static/css/main.d7f9f3d6.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolodex/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89abc366f9dce1ab17a7",
+    "url": "/monsters-rolodex/static/js/main.1029278f.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
